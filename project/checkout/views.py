a41d3b9b6d0ff30e32c726705ea7d24246ab3a9c
Nicholas Kundin <nickundin@gmail.com>
2022-09-26 20:56:55 -0400

    Added header and footer templates

--- project/checkout/views.py
+++ project/checkout/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-  template = loader.get_template('myfirst.html')
+  template = loader.get_template('select_show_time.html')
   return HttpResponse(template.render())
