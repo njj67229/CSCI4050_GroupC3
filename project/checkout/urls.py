c95ba3d5875c19d92ec74ddebfa883d880fc221e
Nicholas Kundin <nickundin@gmail.com>
2022-09-26 21:49:26 -0400

    Added confirmation page, components folder, routin

--- project/checkout/urls.py
+++ project/checkout/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('select-show-time', views.select_show_time, name='select_show_time'),
+    path('confirmation', views.confirmation, name='confirmation'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- project/checkout/views.py
+++ project/checkout/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
+def select_show_time(request):
   template = loader.get_template('select_show_time.html')
+  return HttpResponse(template.render())
+
+def confirmation(request):
+  template = loader.get_template('confirmation.html')
   return HttpResponse(template.render())
