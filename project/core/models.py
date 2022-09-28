aad7258cb92a4af843fe2c3a47fcd92ae5524a48
YaliniNadar <yalini.nadar@gmail.com>
2022-09-28 17:48:17 -0400

    added str field to promo

--- project/core/models.py
+++ project/core/models.py
@@ -40,6 +40,9 @@ class Promo(models.Model):
     discount = models.DecimalField(decimal_places=2, max_digits=3, default=0.1)
     exp_date = models.DateField(default= datetime.now()+ timedelta(days=30))
     
+    def __str__(self):
+        return f"{self.name}"
+    
 
 class Showing(models.Model):
     movie = models.ForeignKey(Movie, on_delete = models.CASCADE)
