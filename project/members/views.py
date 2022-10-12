d8e6dd2a9f5924d26f694c7d15b47868a31f04a7
YaliniNadar <yalini.nadar@gmail.com>
2022-10-12 18:47:18 -0400

    added edit_profile page

--- project/members/views.py
+++ project/members/views.py
@@ -1,12 +1,19 @@
-import imp
-import re
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('index')
+    
+    def get_object(self):
+        return self.request.user
