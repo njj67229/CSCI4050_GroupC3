8c36bbd23ef8c5a9f4a2176707571f4e7fbdd2b2
YaliniNadar <yalini.nadar@gmail.com>
2022-10-13 00:19:20 -0400

    added changing password feature

--- project/members/views.py
+++ project/members/views.py
@@ -1,17 +1,23 @@
+from ast import Pass
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
-from .forms import SignUpForm
-from django.contrib.auth.forms import UserChangeForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
+# from django.contrib.auth.forms import PasswordChangeForm
 
 # Create your views here.
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('login')
+
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('index')
     
