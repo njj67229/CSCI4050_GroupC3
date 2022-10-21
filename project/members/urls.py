87b44f3bae45ddf43dad4aa36b4c8676bbe7e149
YaliniNadar <yalini.nadar@gmail.com>
2022-10-21 11:14:28 -0400

    added forgot password feature

--- project/members/urls.py
+++ project/members/urls.py
@@ -1,3 +1,4 @@
+from re import template
 from django.urls import include, path
 from .views import PasswordsChangeView, UserRegisterView, UserEditView, add_payment, user_login, add_address, add_payment, edit_address, del_address
 from django_email_verification import urls as email_urls
@@ -11,7 +12,21 @@ urlpatterns = [
    path('edit_address/', edit_address, name='edit_address'),
    path('del_address/', del_address, name='del_address'),
    path('add_payment/', add_payment, name='add_payment'),
-   path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html'), name='password'),
+   path('password/', 
+        PasswordsChangeView.as_view(template_name='registration/change-password.html'),
+        name='password'),
+   path('password_reset/', 
+        auth_views.PasswordResetView.as_view(template_name='registration/pswd/password_reset.html'), 
+        name='password_reset'),
+   path('password_reset/done/', 
+        auth_views.PasswordResetDoneView.as_view(template_name='registration/pswd/password_reset_done.html'), 
+        name='password_reset_done'),
+   path('reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='registration/pswd/password_reset_confirm.html'), 
+        name='password_reset_confirm'),
+   path('reset/done/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name='registration/pswd/password_reset_complete.html'),
+        name="password_reset_complete"),
    path('login/', user_login, name='login'),
    path('email/', include(email_urls)),
    
