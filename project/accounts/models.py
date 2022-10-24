6e6a8d3925767474305d092e0bd81e8351ccbb2a
YaliniNadar <yalini.nadar@gmail.com>
2022-10-24 19:44:40 -0400

    ready to change payment cards

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from localflavor.us.models import USStateField
