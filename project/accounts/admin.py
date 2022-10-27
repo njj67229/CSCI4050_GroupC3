25910477f8bd72be2c82f62ed00f826a72144e80
Bella Humphrey <isabelladhumphrey@gmail.com>
2022-10-26 22:27:33 -0400

    css for edit profile and some of login, used black to clean up formatting

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -7,49 +7,89 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, Address, CustomerSatus, PaymentCard
 from core.models import Promo
 from django.core.mail import send_mail
-from django.contrib import messages 
+from django.contrib import messages
 
-@admin.action(description='Send promos to users')
+
+@admin.action(description="Send promos to users")
 def send_promo(modeladmin, request, queryset):
     """Sends Promos to Selected Users"""
     latest_promo = Promo.objects.last()
     email_list = [user.email for user in queryset]
 
     send_mail(
-        'New Promo Code Added',
+        "New Promo Code Added",
         latest_promo.name,
-        'teamc3movies@gmail.com',
+        "teamc3movies@gmail.com",
         email_list,
         fail_silently=False,
     )
-    messages.add_message(request, messages.SUCCESS, 'Emails Have Been Sent')
-    
-    
+    messages.add_message(request, messages.SUCCESS, "Emails Have Been Sent")
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['pk',"email", "username", "first_name", "last_name", "is_superuser", 'status']
-    list_filter = ('status','receive_promos')
+    list_display = [
+        "pk",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_superuser",
+        "status",
+    ]
+    list_filter = ("status", "receive_promos")
     actions = [send_promo]
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('email', 'first_name', 'last_name', 'profile_pic','address', 'paymentcard1','paymentcard2', 'paymentcard3','status', 'receive_promos'),}),
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "profile_pic",
+                    "address",
+                    "paymentcard1",
+                    "paymentcard2",
+                    "paymentcard3",
+                    "status",
+                    "receive_promos",
+                ),
+            },
+        ),
     )
     fieldsets = UserAdmin.fieldsets + (
-        (None,  {'fields': ('address','status', 'profile_pic' ,'receive_promos','paymentcard1','paymentcard2', 'paymentcard3',)}),
+        (
+            None,
+            {
+                "fields": (
+                    "address",
+                    "status",
+                    "profile_pic",
+                    "receive_promos",
+                    "paymentcard1",
+                    "paymentcard2",
+                    "paymentcard3",
+                )
+            },
+        ),
     )
-    
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
 
 @admin.register(Address)
 class Address(admin.ModelAdmin):
     pass
 
+
 @admin.register(PaymentCard)
 class PaymentCard(admin.ModelAdmin):
     pass
+
 
 @admin.register(CustomerSatus)
 class CustomerStatus(admin.ModelAdmin):
