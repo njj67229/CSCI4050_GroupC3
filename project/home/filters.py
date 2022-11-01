56abe8364b49f4916b697d1c9ab6e18b64af79f2
YaliniNadar <yalini.nadar@gmail.com>
2022-11-01 18:46:13 -0400

    added genres filter

--- project/home/filters.py
+++ project/home/filters.py
@@ -1,8 +1,11 @@
-from core.models import Movie
+from core.models import Genre, Movie
 import django_filters
+from django import forms
 
 class MovieFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='startswith', label='')
+    # genres = django_filters.ModelMultipleChoiceFilter(queryset=Genre.objects.all(),
+    #     widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Movie
-        fields = ['title']
+        fields = ['genres','title',]
