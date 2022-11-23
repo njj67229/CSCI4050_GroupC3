037ff16982fb48e2d4574cfd10371e75287decd4
YaliniNadar <yalini.nadar@gmail.com>
2022-11-23 00:01:30 -0500

    added seat selection url param and login reqs

--- project/checkout/urls.py
+++ project/checkout/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
         views.select_tickets_and_age,
         name="select_tickets_and_age",
     ),
+    path("select_seats/<int:show_id>/", views.select_seats, name="select_seats"),
     path("select_seats", views.select_seats, name="select_seats"),
     path("order_summary", views.order_summary, name="order_summary"),
     path("checkout", views.checkout, name="checkout"),
