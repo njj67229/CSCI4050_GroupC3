a0fa641c135a5889ed3ac1976614a968a2865bb1
Nicholas Kundin <nickundin@gmail.com>
2022-12-03 20:53:41 -0500

    Updated gitignore

--- project/load.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system("python3 manage.py loaddata actors.json")
-os.system("python3 manage.py loaddata genres.json")
-os.system("python3 manage.py loaddata mpaa.json")
-os.system("python3 manage.py loaddata new_movies.json")
-os.system("python3 manage.py loaddata physical_seats.json")
-os.system("python3 manage.py loaddata showrooms.json")
-os.system("python3 manage.py loaddata showings.json")
