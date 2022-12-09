0e2a67de774eaf6c7e106af1a3d235a491a81324
njj67229 <njj67229@uga.edu>
2022-12-09 17:34:02 -0500

    Load.py changes

--- project/load.py
+++ project/load.py
@@ -5,4 +5,7 @@ os.system("python3 manage.py loaddata genres.json")
 os.system("python3 manage.py loaddata mpaa.json")
 os.system("python3 manage.py loaddata new_movies.json")
 os.system("python3 manage.py loaddata physical_seats.json")
-# os.system("python3 manage.py loaddata showrooms.json")
+os.system("python3 manage.py loaddata showrooms.json")
+os.system("python3 manage.py loaddata showing_seats.json")
+os.system("python3 manage.py loaddata showings.json")
+
